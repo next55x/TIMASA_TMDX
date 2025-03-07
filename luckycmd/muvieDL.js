b307133215a962e012d20a795d09ee7852eda7a4
next55x <deploytech597@gmail.com>
2025-03-07 16:31:03 +0300

    Update muvieDL.js

--- luckycmd/muvieDL.js
+++ luckycmd/muvieDL.js
@@ -3,7 +3,7 @@ const { getJson } = require("../fredi/utils");
 
 ezra({
   nomCom: "movie",
-  categorie: "luckymd-search",
+  categorie: "Timnasamd-search",
   reaction: "🎥"
 }, async (origineMessage, zk, commandeOptions) => {
   const { repondre, arg } = commandeOptions;
