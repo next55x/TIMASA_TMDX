297ee8dc63ecca8c2e0f12df1f0f1bcbe2d1d028
next55x <deploytech597@gmail.com>
2025-03-08 06:32:44 +0300

    Update zsurn.js

--- luckycmd/zsurn.js
+++ luckycmd/zsurn.js
@@ -46,7 +46,7 @@ ezra({
     
     const data = response.data.data;
     const messageText = `
-ᬑ *LUCKY QURAN SURAH* ᬒ
+ᬑ *TIMNASA QURAN SURAH* ᬒ
 
 *🕌 Quran: The Holy Book*
 📜 *Surah:* 🕌❤️${data.number}: ${data.asma.ar.long} (${data.asma.en.long})❤️🕌
@@ -62,7 +62,7 @@ ezra({
       text: messageText,
       contextInfo: {
         externalAdReply: {
-          title: "LUCKY QURAN SURAH",
+          title: "TIMNASA QURAN SURAH",
           body: `We're reading: ${data.asma.en.long}`,
           mediaType: 1,
           thumbnailUrl: conf.URL, 
@@ -79,7 +79,7 @@ ezra({
         externalAdReply: {
           title: "Error Fetching Quran Passage",
           body: "Please try again later.",
-          thumbnailUrl: "https://files.catbox.moe/058ggv.jpg", // Replace with a suitable thumbnail URL
+          thumbnailUrl: "https://files.catbox.moe/e8tdel.jpg", // Replace with a suitable thumbnail URL
           sourceUrl: conf.GURL,
           mediaType: 1,
           showAdAttribution: true,
